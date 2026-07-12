7e718e7df8e20aad1bb8acae16c21b6956283762
Super son <subarshan195@gmail.com>
2026-07-12 11:30:45 +0530

    feat: add multiple student entries with Google Docs embed IDs

--- src/data/students.js
+++ src/data/students.js
@@ -8,7 +8,96 @@ export const students = [
     slug: 'test-student',
     docId: '2PACX-1vTurOk-K2lk2x3ejMiUP6dik5eGR6bO7tnWUFFZjqCAxnNSBjNHBgzpSNTli8un1JWRTrp0gOM70Dg-'
   },
-  // Add more students here:
+  {
+    name: 'Pavithramenon',
+    slug: 'pavithramenon',
+    docId: '2PACX-1vSWMows4JOiRjuwHcjT-6K5SSi6o3uaBpYCjSrdag1yVZirmUq4jQwgucBMuJNOLqd47zvF6y161YfF'
+  },
+  {
+    name: 'Nimimenon',
+    slug: 'nimimenon',
+    docId: '2PACX-1vRuViS5Y6BIv0vdQoVnTwspZBjfYTRSipl8yWcXyBrsoOn2S8mZ0H4CZ5GFNjYNXga8ageIiP-xIuNi'
+  },
+  {
+    name: 'Noah',
+    slug: 'noah',
+    docId: '2PACX-1vStCo5K1_52FNuW9CGKshCHa4LZniUnb183f9jnxQLpBiRsyF6m-qKVlHEJRkCjQTkPhFHy3-JF1HpZ'
+  },
+  {
+    name: 'Nathan',
+    slug: 'nathan',
+    docId: '2PACX-1vRYFmrzq66JrJZgsrFXBbiGo706yL30FJe4nD0hBBcqP2Cm5Uxdbqjs8SYIoA2RGxX3MZge7jiWag5L'
+  },
+  {
+    name: 'Ishana',
+    slug: 'ishana',
+    docId: '2PACX-1vSaOxJRCw_9XCJQaX18-QYnVbjQ0XmaDfBXvmA4DFnF3P70WWqKUXmwvOOX-913oKbk-h5l9Y67WuoV'
+  },
+  {
+    name: 'Ananya',
+    slug: 'ananya',
+    docId: '2PACX-1vRqS3q4grMlKN3RO1gyG6SUdH8EQhJDYNrCZPZqukxRTKoHy3Sod17kEpov836JFlUCqZ5Tmw3F99Sc'
+  },
+  {
+    name: 'Vamika',
+    slug: 'vamika',
+    docId: '2PACX-1vSoMpCUBpY3_WYEZAAlrGW4TCwShCbtMh0lVRS_3bvOqJkY5A3B5dxfQjKoO0rF2Hswa6sK2Z9T7wqy'
+  },
+  {
+    name: 'Tamiksha',
+    slug: 'tamiksha',
+    docId: '2PACX-1vSQY9XnBVhomN5I4c5be3AtFIUdUBxd74sMXzavs4DaYyBm1N5x4tx1SENALbnp5xkuizuKARbSNiF9'
+  },
+  {
+    name: 'Thanish',
+    slug: 'thanish',
+    docId: '2PACX-1vTOd6N2SHTEx0hnY1V6vqMrXgWkR3PpMHEsICiECWDFquPSBamT6Y_lejjQzFaio7mpCw-Du16zzAMg'
+  },
+  {
+    name: 'Annette',
+    slug: 'annette',
+    docId: '2PACX-1vTpiW3It-OdyxfYqnF4NnXy6NKcehBcIOYuMWf91s266RUFifR_rPuiCJZ2leoQac3v_1WVf5y6zk9h'
+  },
+  {
+    name: 'Emmanuel',
+    slug: 'emmanuel',
+    docId: '2PACX-1vRxPwZwoHVjNl9qA_nf5ifYKxq8hLHQdzJV6ymgXN5ZoAYYeDtxLZCB5o6zRYi_CRRMdW_fQwzelzdZ'
+  },
+  {
+    name: 'Katherine',
+    slug: 'katherine',
+    docId: '2PACX-1vQ4NK6-BVasmWTnzL5OL337zNxMhdfGtn9o6PgugVbM11m3Haa5rWT1TWl-cOshW6NhG2c_mc46wMCC'
+  },
+  {
+    name: 'Evelyn',
+    slug: 'evelyn',
+    docId: '2PACX-1vQaErYc0A3dIl9-RnULsdGhlws109gH8M70Cf2YYl7zKwefCv7iRWWklAdV2zYD3tUHUVOpQNad6rru'
+  },
+  {
+    name: 'Madhumitha',
+    slug: 'madhumitha',
+    docId: '2PACX-1vR87AFFBvlULcYrxCfV3aWl62gMpxCeosUWDkr2P-MlqlSJb11fiymbPbhphzBTog8EveY-aPhdSmR7'
+  },
+  {
+    name: 'Aahanaa',
+    slug: 'aahanaa',
+    docId: '2PACX-1vSKxG_vSl8tPh2XtIjWdNI3r-OdKrcO5xp0Uf7Jf6NJaD2XrwkLSpUWXdteGFp35lP9kdFOLCxC4qDZ'
+  },
+  {
+    name: 'Mikhael',
+    slug: 'mikhael',
+    docId: '2PACX-1vSHVviijtawe000VNntQLf43zmH2H-uJPwKk6zl5x0CXVIsDk3T5UA3AEBAh0Jx5TTIe7hiQH2B-2go'
+  },
+  {
+    name: 'Newstudent1',
+    slug: 'newstudent1',
+    docId: '2PACX-1vR6O8bUMeeOfpRwWtF5-GVyI34QjMieHxqqyRZ7gXCkU_tR87fcwUdEsJOu0-opeiMQ4xn12mEucdya'
+  },
+  {
+    name: 'Newstudent2',
+    slug: 'newstudent2',
+    docId: '2PACX-1vSYiQ0xG5j7Nn_iMh4llSuuICo7mN-VVVjm4kwI1D4Ny0Ccyd7lAgNmAsmTUArQTvVYAajQeqVwXHHk'
+  },
   // {
   //   name: 'Jane Smith',
   //   slug: 'jane-smith',
